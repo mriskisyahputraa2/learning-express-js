2837b0d2776408c7b7e138e42d86cbf5cb096875
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-05-14 14:57:05 +0700

    update

--- 03-view-enggine/src/routes/index.js
+++ 03-view-enggine/src/routes/index.js
@@ -2,11 +2,15 @@ import express from "express";
 const router = express.Router();
 import barangRouter from "./barang.js";
 
+router.use("/", (req, res) => {
+  res.render("index");
+});
+
 router.use("/barangs", barangRouter);
 
-router.all("/barangs", (req, res) => {
-  res.send("all methods barang");
-});
+// router.all("/barangs", (req, res) => {
+//   res.send("all methods barang");
+// });
 router.use("*", (req, res) => {
   res.status(404);
   res.send("404 Not Found");
