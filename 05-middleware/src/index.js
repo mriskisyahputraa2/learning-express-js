44ff4b40e4644b381d7697e66f971cd6647da740
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-05-16 11:34:57 +0700

    update

---
+++ 05-middleware/src/index.js
@@ -0,0 +1,24 @@
+import express from "express"; // import express
+const app = express();
+const port = 3000;
+
+import router from "./routes/index.js"; // import router
+
+import expressEjsLayouts from "express-ejs-layouts"; // import expressEjsLayouts
+import path from "path"; // import path
+import url from "url"; // import url
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url)); // deklarasi __dirname
+
+// meng-set path dari views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
+
+// digunakan untuk mengakses folder public yang tesimpan semua assets
+app.use("/static", express.static(path.join(__dirname, "../public")));
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
