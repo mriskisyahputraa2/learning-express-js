642c9dec9cdf0ce78b05f4ce472c0d717326afaa
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-05-19 11:40:57 +0700

    update

--- 07-cookies/src/index.js
+++ 07-cookies/src/index.js
@@ -1,15 +1,30 @@
 // materi cookies
-
 import express from "express";
+import cookieParser from "cookie-parser"; // import cookieParser
 
 const app = express();
 const port = 3000;
+
+// mendefinisikan cookie parser
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   console.log("hallo");
   res.send("hallo");
 });
 
+app.get("/set-cookies", (req, res) => {
+  res.send("Cookies are set");
+});
+
+app.get("/get-cookies", (req, res) => {
+  res.send(req.cookies);
+});
+
+app.get("/delete-cookies", (req, res) => {
+  res.clearCookie("age");
+});
+
 app.listen(port, () => {
-  console.log("port listening on port" + port);
+  console.log("port listening on port " + port);
 });
