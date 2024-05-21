523acfcbc0d38b1b7e846eb2839797d45d53a413
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-05-21 10:26:15 +0700

    update

---
+++ 09-login-aut/routes/index.js
@@ -0,0 +1,20 @@
+import express, { Router } from "express";
+const routes = express.Router(); // definisi routes express router
+
+let Users = []; // membuat array untuk menyimpan data dummy
+
+// routes
+routes.get("/", (req, res) => {
+  res.send("Hello brayy");
+});
+
+routes.get("/singup", (req, res) => {
+  const data = {
+    title: "Sing Up",
+    layout: "layout/main-layout",
+    message: "",
+  };
+  res.render(data);
+});
+
+export default routes;
