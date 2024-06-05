217c68c96a4812230aa46d49318d6db520209ef7
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-06-05 10:11:56 +0700

    13 arsitektur mvc add controllers & middleware selesai

--- 13-arsitektur-mvc/src/index.js
+++ 13-arsitektur-mvc/src/index.js
@@ -1,42 +1,18 @@
 import express from "express";
 import routes from "../routes/index.js"; // import routes dari routes/index.js
+import appMiddleware from "./middleware/index.js";
 
 const app = express();
 const port = 3000;
 
-import bodyParser from "body-parser"; // Untuk mem-parsing badan request
-import multer from "multer"; // Untuk menangani form data, khususnya file upload
-import cookieParser from "cookie-parser"; // Untuk mem-parsing cookie
-import session from "express-session"; // Untuk mengelola session
-import expressEjsLayouts from "express-ejs-layouts"; // Untuk mengelola layout EJS
-import flash from "express-flash"; // Untuk message kesalahan / error
-
 import path from "path"; // Untuk mengelola path file dan direktori
 import url from "url"; // Untuk mengelola URL
-const upload = multer();
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 app.set("views", path.join(__dirname, "views")); // Mengatur direktori views ke dalam folder views.
 app.set("view engine", "ejs"); // Mengatur EJS sebagai template engine.
 
-app.use(expressEjsLayouts); // Mengaktifkan layout EJS.
-app.use(express.static(path.join(__dirname, "../public"))); // Menyajikan file statis dari direktori public.
-
-app.use(bodyParser.json()); // Mengaktifkan parsing JSON.
-app.use(bodyParser.urlencoded({ extended: true })); // Mengaktifkan parsing URL-encoded data.
-app.use(upload.array()); // Mengaktifkan upload file dengan Multer.
-app.use(cookieParser()); //  Mengaktifkan parsing cookie.
-
-// konfigurasi untuk middlaware session
-app.use(
-  session({
-    secret: "secret key",
-    resave: false,
-    saveUninitialized: false, //
-  })
-);
-
-app.use(flash()); // menggunakan route middleware nya
+app.use(appMiddleware); // menggunakan segala macam middleware
 app.use(routes); // menggunakan routes middleware nya
 
 app.listen(port, () => {
