f9fa243f5bc0fbe0cdbab37e94bf6e7a0b6e00af
mriskisyahputraa2 <riskideveloper2@gmail.com>
2024-06-13 13:02:56 +0700

    14-logging selesai

--- 14-logging/src/logs/winston.js
+++ 14-logging/src/logs/winston.js
@@ -1,38 +1,45 @@
-import winston from "winston";
-import "winston-daily-rotate-file";
-import "winston-mongodb";
-import TransportStream from "winston-transport";
+import winston from "winston"; // import winston library utama untuk logging
+import "winston-daily-rotate-file"; // untuk transportasi file log baru setiap hari/setiap saat
+import "winston-mongodb"; // library transportasi untuk menyimpan log ke mongodb
+import TransportStream from "winston-transport"; // library untuk membuat transportasi costum
 
 // keneksi ke database mongodb
-import mongodb from "mongodb";
-const MongoClient = mongodb.MongoClient;
-const url = "mongodb://127.0.0.1:27017/latihan";
-const client = new MongoClient(url);
-await client.connect();
+import mongodb from "mongodb"; // library mongodb untuk  menghubungkan ke database mongodb
+const MongoClient = mongodb.MongoClient; // koneksi ke database mongodb
+const url = "mongodb://127.0.0.1:27017/latihan"; // url/localhost koneksi ke database mongodb dgn nama database latihan
+const client = new MongoClient(url); // menyimpan url/localhost ke dalam variabel client
+await client.connect(); // menghubungkan ke database mongodb
 
+// membuat class transportasi costum
+// MyClass adalah kelas yang meng-extend TransportStream untuk membuat transportasi log kustom.
 class MyClass extends TransportStream {
+  // constructor(options): Konstruktor yang menerima options dan memanggil konstruktor parent TransportStream.
   constructor(options) {
     super(options);
   }
 
   // custom disini sesuai kebutuhan email, WA, dsb ....
   log(info, next) {
+    // Metode log dipanggil setiap kali ada pesan log. Dalam contoh ini, pesan log dicetak ke console dengan format tanggal, level log, dan pesan log.
     console.log(`${new Date()} ${info.level} ${info.message}`);
     next();
   }
 }
 
+// konfigurasi transportasi DailyRotateFile, untuk menyimpan log ke file yang berotasi setiap jam
 const transportsDailyRotateFile = new winston.transports.DailyRotateFile({
-  level: "silly",
-  filename: "./logs/app-%DATE%.log",
-  datePattern: "YYYY-MM-DD-HH",
-  zippedArchive: true,
-  maxSize: "1m",
-  maxFiles: "14d",
+  level: "silly", // menentukan level log terendah yaitu silly
+  filename: "./logs/app-%DATE%.log", // Format nama file untuk file log, menggunakan placeholder %DATE% yang akan digantikan dengan tanggal.
+  datePattern: "YYYY-MM-DD-HH", // Pola tanggal untuk mengganti %DATE% dalam nama file.
+  zippedArchive: true, //  Log akan diarsipkan dalam format zip.
+  maxSize: "1m", // Ukuran maksimal file log adalah 1MB.
+  maxFiles: "14d", // Log akan disimpan maksimal selama 14 hari.
 });
 
+// membuat logger dengan winston
 const logger = winston.createLogger({
-  level: "silly",
+  level: "silly", // Logger ini akan mencatat semua pesan log dari level silly ke atas.
+  //  Menggabungkan beberapa format log seperti JSON, timestamp, label, dan string yang diformat.
   format: winston.format.combine(
     winston.format.json({ space: 2 }),
     winston.format.timestamp({
@@ -44,22 +51,28 @@ const logger = winston.createLogger({
         `${info.label}, ${info.timestamp}, ${info.level} : ${info.message}`
     )
   ),
+
+  // menentukan tempat log yg akan disimpan
   transports: [
     // new winston.transports.Console({
     //   level: "silly",
     //   format: winston.format.combine(winston.format.colorize({ all: true })),
     // }),
+
+    // File transportasi menyimpan log ke file ./logs/app.log
     new winston.transports.File({
-      handleExceptions: true,
+      handleExceptions: true, // jika ada kesalahan akan memunculkan pesan error
       level: "silly",
       filename: "./logs/app.log",
     }),
+
+    // dan ./logs/app-error.log.
     new winston.transports.File({
       handleExceptions: true,
       level: "error",
       filename: "./logs/app-error.log",
     }),
-    transportsDailyRotateFile,
+    transportsDailyRotateFile, //  Menyimpan log harian yang berputar.
     // new winston.transports.MongoDB({
     //   level: "error",
     //   db: await Promise.resolve(client),
@@ -67,9 +80,10 @@ const logger = winston.createLogger({
     //   capped: true,
     // }),
 
+    // Transportasi kustom yang mencetak log ke console.
     new MyClass({
       level: "silly",
-      format: winston.format.combine(winston.format.colorize({ all: true })),
+      format: winston.format.combine(winston.format.colorize({ all: true })), // kombinasi data log yang masuk dengan menambahkan warna
     }),
   ],
 });
